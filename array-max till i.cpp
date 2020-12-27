2aa35c433a1bc29b7b0b7819ea96d38827f4ea9c
yashu3014 <76508094+yashu3014@users.noreply.github.com>
2020-12-27 14:48:12 +0530

    Add files via upload

---
+++ array-max till i.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+   int mx=-199999999;
+    cin>>n;
+    
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+    cin>>arr[i];
+    }
+    
+    for(int i=0;i<n;i++)
+    {
+    mx=max(mx, arr[i]);
+    cout<<mx<<"\n";
+    }
+    
+    
+    return 0;
+}
